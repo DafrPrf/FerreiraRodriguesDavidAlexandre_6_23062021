583d27dd22051baa6e02b939a6937dbdfcb4f4b7
Dafr <dafr.prf@gmail.com>
2021-07-07 12:54:41 +0200

    working on add/remove likes

--- controllers/sauce.js
+++ controllers/sauce.js
@@ -59,8 +59,36 @@ exports.getAllSauces = (req, res, next) => {
 
 exports.likes = (req, res, next) => {
   const userId = req.body.userId;
-  const like = req.body.likes;
-  const dislikes = req.body.dislikes;
+  const like = req.body.like;
 
-  console.log(userId);
+  Sauce.findOne({ _id: req.params.id })
+    .then((sauce) => {
+      if (like === 1 && !sauce.usersLiked.includes(userId)) {
+        Sauce.updateOne({
+          $inc: { likes: 1 },
+          $push: { usersLiked: userId },
+        })
+          .then(() => res.status(201).json({ message: 'liked' }))
+          .catch((err) => res.status(400).json({ err }));
+      }
+
+      if (like === 0 && sauce.likes > 0 && sauce.usersLiked.includes(userId)) {
+        Sauce.updateOne({
+          $inc: { likes: -1 },
+          $pull: { usersLiked: userId },
+        })
+          .then(() => res.status(201).json({ message: 'like removed' }))
+          .catch((err) => res.status(400).json({ err }));
+      }
+
+      if (like === -1) {
+        Sauce.updateOne({
+          $inc: { likes: 1 },
+          $push: { usersDisliked: userId },
+        })
+          .then(() => res.status(201).json({ message: 'disliked' }))
+          .catch((err) => res.status(400).json({ err }));
+      }
+    })
+    .catch((err) => res.status(404).json({ err }));
 };
